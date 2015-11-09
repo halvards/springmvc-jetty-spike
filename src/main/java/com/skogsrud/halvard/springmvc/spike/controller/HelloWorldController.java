fc803f66a481a27caa6b22dffa0975ae229ad196
Halvard Skogsrud <halvard@skogsrud.com>
2015-11-09 23:28:04 +1100

    Controller handler methods with JSON and Mustache templating

--- src/main/java/com/skogsrud/halvard/springmvc/spike/controller/HelloWorldController.java
+++ src/main/java/com/skogsrud/halvard/springmvc/spike/controller/HelloWorldController.java
@@ -3,11 +3,29 @@ package com.skogsrud.halvard.springmvc.spike.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class HelloWorldController {
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @RequestMapping(value = "/hello", method = RequestMethod.GET, produces = "text/plain")
     public String hello() {
         return "Hello world";
+    }
+
+    @RequestMapping(value = "/hello.json", method = RequestMethod.GET, produces = "application/json")
+    public Map<String, Object> helloJson() {
+        return new HashMap<String, Object>() {{
+            put("hello", "world");
+        }};
+    }
+
+    @RequestMapping(value = "/hello.html", method = RequestMethod.GET, produces = "text/html")
+    public ModelAndView helloHtml() {
+        return new ModelAndView("helloworld", new HashMap<String, Object>() {{
+            put("hello", "world");
+        }});
     }
 }
